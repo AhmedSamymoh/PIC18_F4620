372d39d0b5e6ca3bb5ca98dcc82fcc824980d7d4
Ahmed Samy El Nozahy <108683164+AhmedSamymoh@users.noreply.github.com>
2023-09-14 07:10:56 +0300

    Initial commit

--- application.c
+++ application.c
@@ -9,45 +9,76 @@
 
 Std_ReturnType ret = E_NOT_OK;
 uint8 keypad_val = 0 ;
+uint8 time = 0 ;
+uint8 time_str[4];
+uint8 customChar1[] = {0x0E , 0X0A , 0X11 , 0X11 , 0X11 , 0X1F , 0X00};
+uint8 customChar2[] = {0x0E , 0X0A , 0X11 , 0X11 , 0X1F , 0X1F , 0X00};
+uint8 customChar3[] = {0x0E , 0X0A , 0X11 , 0X1F , 0X1F , 0X1F , 0X00};
+uint8 customChar4[] = {0x0E , 0X0A , 0X1F , 0X1F , 0X1F , 0X1F , 0X00};
+uint8 customChar5[] = {0x0E , 0X0A , 0X1F , 0X1F , 0X1F , 0X1F , 0X00};
+uint8 customChar6[] = {0x0E , 0X0E , 0X1F , 0X1F , 0X1F , 0X1F , 0X00};
+
+
+void loading()
+{
+    ret = lcd_4bit_send_string_pos(&lcd_1 ,1,5,"Loading");
+    __delay_ms(250);
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        ret = lcd_4bit_send_char_data(&lcd_1 , '.');
+        __delay_ms(250);
+
+    }
+    ret = lcd_4bit_send_string_pos(&lcd_1 ,1,5,"            ");
+ 
+}
+void battery(void)
+{   
+    ret = lcd_4bit_send_command(&lcd_1 , _LCD_CLEAR); 
+    ret = lcd_4bit_send_string_pos(&lcd_1 ,1,3,"Charging Battery");
+    for(int i = 0 ; i < 100 ; i++)
+    {
+        ret = conv_byte_to_string(i ,time_str );
+        ret = lcd_4bit_send_string_pos(&lcd_1 ,3,9, time_str);
+        ret = lcd_4bit_send_string_pos(&lcd_1 ,3,13, "%");
+        __delay_ms(250);
+
+    }
+    time_str[4] = "000";
+    for(int i = 0 ; i < 12 ; i++)
+    {
+        ret = lcd_4bit_send_command(&lcd_1 , _LCD_CLEAR);
+        __delay_ms(250);
+
+        ret = lcd_8bit_send_command(&lcd_1 , (0x94 + 2) );
+                __delay_ms(250);
+ 
+        ret = lcd_4bit_send_string_pos(&lcd_1 ,3,3, "BATTERY CHARGED !!");
+        __delay_ms(250);
+        ret = lcd_8bit_send_custom_char(&lcd_2 , 4, 10 ,customChar4 , 0);
+
+
+    }
+   
+
+}
 
 int main(){
     Std_ReturnType ret = E_NOT_OK;
     application_intialize();
+    
     while(1)
     {
-        ret = keypad_get_value(&keypad1 , &keypad_val);
-        switch(keypad_val)
-        {
-            case '1':
-                ret = seven_segment_write_number(&seg1 , 1);
-                break;
-            case '2':
-                ret = seven_segment_write_number(&seg1 , 2);
-                break;
-            case '3':
-                ret = seven_segment_write_number(&seg1 , 3);
-                break;
-            case '4':
-                ret = seven_segment_write_number(&seg1 , 4);
-                break;
-            case '5':
-                ret = seven_segment_write_number(&seg1 , 5);
-                break;
-            case '6':
-                ret = seven_segment_write_number(&seg1 , 6);
-                break;
-            case '7':
-                ret = seven_segment_write_number(&seg1 , 7);
-                break;
-            case '8':
-                ret = seven_segment_write_number(&seg1 , 8);
-                break;
-            case '9':
-                ret = seven_segment_write_number(&seg1 , 9);
-                break;
-            default :
-                break;
-        }
+        //battery();
+        ret = lcd_8bit_send_custom_char(&lcd_2 , 4 ,10 ,customChar1 , 0);
+        __delay_ms(250);
+        ret = lcd_8bit_send_custom_char(&lcd_2 , 4 ,10 ,customChar2 , 0);
+        __delay_ms(250);
+        ret = lcd_8bit_send_custom_char(&lcd_2 , 4 ,10 ,customChar3 , 0);
+        __delay_ms(250);
+        ret = lcd_8bit_send_custom_char(&lcd_2 , 4 ,10 ,customChar4 , 0);
+        __delay_ms(250);
+
 
     } 
     return (EXIT_SUCCESS);
