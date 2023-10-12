9eeb246ac2e3db34ca198d91c04196edcbc5b266
Ahmed Samy El Nozahy <108683164+AhmedSamymoh@users.noreply.github.com>
2023-10-12 03:40:58 +0300

    Update EEPROM Software module

--- Apps/09 - EEPROM/application.c
+++ Apps/09 - EEPROM/application.c
@@ -6,21 +6,74 @@
  */
 #include "application.h"
 
+void isr_int0();
 Std_ReturnType ret = E_NOT_OK;
-int val = 0 , read = 0;
+
+volatile uint8 Program_Select = 0 ;
+uint8 btn_State = 0  , high_Valid = 0  , btn_State_Valid = 0 , last_btn_State_Valid = 0;
+
+Led_t led1 = {.port_name = PORTC_INDEX , .pin = GPIO_PIN0 , .led_status = LED_OFF };
+Led_t led2 = {.port_name = PORTC_INDEX , .pin = GPIO_PIN1 , .led_status = LED_OFF };
+
+interrupt_INTx_t  int0 = {
+    .EXT_InterruptHandler = isr_int0 ,
+    .edge = INTERRUPT_RISING_EDGE ,  
+    .mcu_pin.port = PORTB_INDEX,
+    .mcu_pin.pin = GPIO_PIN0,
+    .mcu_pin.direction = GPIO_DIR_INPUT,
+    .mcu_pin.logic = GPIO_LOW,
+    .source = EXTERNAL_INT0,
+ 
+};
+
+void isr_int0(){
+    Program_Select++;
+    ret = EEPROM_WriteByte(0x01B, Program_Select);
+}
+void Program1(void){
+    led_TURN_ON(&led1);
+    __delay_ms(1000);
+    led_TURN_OFF(&led1);
+    __delay_ms(1000);
+}
+void Program2(void){
+    led_TURN_ON(&led2);
+    __delay_ms(1000);
+    led_TURN_OFF(&led2);
+    __delay_ms(1000);
+}
+void Program3(void){
+    led_TURN_ON(&led1);
+    led_TURN_ON(&led2);
+    __delay_ms(1000);
+    led_TURN_OFF(&led1);
+    led_TURN_OFF(&led2);
+    __delay_ms(1000);
+}
 
 int main(){
-    
+     
     application_intialize();
-    ret = EEPROM_WriteByte(0x3ff , 0);
-    ret = EEPROM_WriteByte(0x0260 , 1234);
+    ret = interrupt_INTx_Init(&int0);
+
+    ret = EEPROM_ReadByte(0x01B , &Program_Select);
+    
     while(1)
     {
-        ret = EEPROM_WriteByte(0x3ff , val++);
-        __delay_ms(1000);
-        ret = EEPROM_ReadByte(0x3ff , &read);
-        __delay_ms(1000);
-
+        if (1 == Program_Select){
+            Program1();
+        }else if (2 == Program_Select){
+            Program2();
+        }else if (3 == Program_Select){
+            Program3();
+        }else{
+            led_TURN_OFF(&led1);
+            led_TURN_OFF(&led2);
+            
+            if(Program_Select >= 4)
+                Program_Select = 0;
+        }
+    
     } 
     return (EXIT_SUCCESS);
 }
@@ -28,5 +81,7 @@ int main(){
 void application_intialize(void){
     Std_ReturnType ret = E_NOT_OK;
     //ecu_layer_intialize();
+    ret = led_init(&led1);
+    ret = led_init(&led2);
 
 } 
